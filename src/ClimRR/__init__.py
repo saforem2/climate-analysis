b35c375a0d916d151de5bbd70a2e5e564323537a
Sam Foreman <saforem2@gmail.com>
2023-07-07 08:47:31 -0500

    Adds `src/ClimRR/*`

---
+++ src/ClimRR/__init__.py
@@ -0,0 +1,10 @@
+"""
+src/climrr/__init__.py
+"""
+from __future__ import absolute_import, annotations, division, print_function
+
+from pathlib import Path
+
+HERE = Path(__file__).parent
+ROOT = HERE.parent.parent
+DATA_DIR = ROOT / "data"
